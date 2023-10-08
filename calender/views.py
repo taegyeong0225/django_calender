efb6a38ae10fc718b0aae491f91b3be6657129ea
taegyeong0225 <202244042@itc.ac.kr>
2023-10-08 16:59:00 +0900

    DB 연동 성공, month-list (week는 연동 문제로 삭제)

--- calender/views.py
+++ calender/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from calender.models import Events
 from django.http import JsonResponse
+from calender.models import Events
 
 def index(request):
     all_events = Events.objects.all()
@@ -12,13 +12,44 @@ def index(request):
     return render(request, 'landing.html', context)
 
 def all_events(request):
-    all_events = Events.objects.all()
+    all_events = Events.objects.all() # 모든 이벤트 받아옴
     out = []
     for event in all_events:
         out.append({
             'title': event.name,
             'id': event.id,
-            'start': event.start.strftime("%m/%d/%Y", "%H:%M:%S"),
-            'end': event.end.strftime("%m/%d/%Y", "%H:%M:%S"),
+            'start': event.start.strftime("%m/%d/%Y %H:%M:%S"),
+            'end': event.end.strftime("%m/%d/%Y" "%H:%M:%S"),
         })
-        return JsonResponse(out, safe=False)
+    return JsonResponse(out, safe=False) # 쿨라이언트한테 전달
+
+def add_event(request):
+    start = request.GET.get("start", None)
+    end = request.GET.get("end", None)
+    title = request.GET.get("title", None)
+    event = Events(name=str(title), start=start, end=end)
+    event.save()
+    data = {}
+    return JsonResponse(data)
+
+
+def update(request):
+    start = request.GET.get("start", None)
+    end = request.GET.get("end", None)
+    title = request.GET.get("title", None)
+    id = request.GET.get("id", None)
+    event = Events.objects.get(id=id)
+    event.start = start
+    event.end = end
+    event.name = title
+    event.save()
+    data = {}
+    return JsonResponse(data)
+
+
+def remove(request):
+    id = request.GET.get("id", None)
+    event = Events.objects.get(id=id)
+    event.delete()
+    data = {}
+    return JsonResponse(data)
--- django_calender_prj/urls.py
+++ django_calender_prj/urls.py
@@ -21,5 +21,8 @@ from calender import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('all_events/', views.all_events, name="all_events")
+    path('all_events/', views.all_events, name='all_events'),
+    path('add_event/', views.add_event, name='add_event'),
+    path('update/', views.update, name='update'),
+    path('remove/', views.remove, name='remove'),
 ]
