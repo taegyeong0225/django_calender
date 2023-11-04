b667f013dec9493b534741c99baf10705c39e057
taegyeong0225 <202244042@itc.ac.kr>
2023-11-04 21:56:08 +0900

    Revert "foreignKey user 추가, 처리 함수 views.py 수정"
    This reverts commit 32d98bffaeacbe627aaac1e4dab990484a7d0121.

--- calender/models.py
+++ calender/models.py
@@ -1,24 +1,11 @@
-# models.py
 from django.db import models
-from django.contrib.auth.models import User
 
+# Create your models here.
 class Events(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default="None")  # Relate each event to a specific user
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, null=False, blank=False, default="입력 필요")
-    end = models.DateTimeField(null=True, blank=True)  # blank=True로 수정
-    category = models.CharField(
-        max_length=20,
-        choices=[
-            ('food', '재료'),
-            ('no-food', '비재료'),
-            ('none', '선택하지 않음'),  # 추가 옵션
-        ],
-        null=False,
-        blank=False,  # blank=True에서 blank=False로 수정
-        default='none'  # 기본값 설정
-    )
-
+    name = models.CharField(max_length=255,null=True,blank=True)
+    start = models.DateTimeField(null=True,blank=True)
+    end = models.DateTimeField(null=True,blank=True)
 
     class Meta:
         db_table = "tblevents"
