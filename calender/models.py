0879d09826c383340a61ce301e89c54ff4c852ea
taegyeong0225 <202244042@itc.ac.kr>
2023-11-04 21:24:12 +0900

    Save changes before reverting

--- calender/models.py
+++ calender/models.py
@@ -1,20 +1,22 @@
 # models.py
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 class Events(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255,null=False,blank=False, default="입력 필요")
-    end = models.DateTimeField(null=True,blank=False)
+    name = models.CharField(max_length=255, null=False, blank=False, default="입력 필요")
+    end = models.DateTimeField(null=True, blank=True)  # blank=True로 수정
     category = models.CharField(
-        max_length=20,  # 적절한 최대 길이 설정
+        max_length=20,
         choices=[
-            ('food', '재료'),  # HTML select의 value 및 표시 텍스트
+            ('food', '재료'),
             ('no-food', '비재료'),
+            ('none', '선택하지 않음'),  # 추가 옵션
         ],
         null=False,
-        blank=True
+        blank=False,  # blank=True에서 blank=False로 수정
+        default='none'  # 기본값 설정
     )
 
 
