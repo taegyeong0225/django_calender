89ed9e83a28a384a1a09bd4b0397d231adb658af
taegyeong0225 <202244042@itc.ac.kr>
2023-11-05 18:39:28 +0900

    DB 모델의 User 테이블 연결

--- calender/models.py
+++ calender/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User # user 연결
 
 # Create your models here.
 class Events(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255,null=True,blank=True)
     start = models.DateTimeField(null=True,blank=True)
     end = models.DateTimeField(null=True,blank=True)
+
+
 
     class Meta:
         db_table = "tblevents"
