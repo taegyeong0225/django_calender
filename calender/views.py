3bec02879162c95453c6dcfe88b4396a76a0c419
taegyeong0225 <202244042@itc.ac.kr>
2023-11-08 01:17:07 +0900

    로그인을 안해도 대문페이지 접근 가능 but 이벤트 추가 시도시 제한

--- calender/views.py
+++ calender/views.py
@@ -3,38 +3,37 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.http import HttpResponseForbidden
 from django.shortcuts import get_object_or_404
-from calender.models import Events
+from .models import Events
 
+
+# @login_required(login_url='accounts/login/')  # 로그인하지 않은 사용자를 로그인 페이지로 리다이렉트합니다.
 # def index(request):
-#     # 사용자가 로그인했는지 확인합니다.
-#     if request.user.is_authenticated:
-#         # 로그인한 사용자의 이벤트만 필터링합니다.
-#         user_events = Events.objects.filter(user=request.user)
-#     else:
-#         # 비로그인 사용자에게는 이벤트를 보여주지 않습니다.
-#         user_events = Events.objects.none()
+#     # 현재 로그인한 사용자와 연결된 이벤트들만 가져옵니다.
+#     user_events = Events.objects.filter(user=request.user)
 #
 #     context = {
 #         "events": user_events,
 #     }
 #     return render(request, 'calendar.html', context)
 
-@login_required(login_url='accounts/login/')  # 로그인하지 않은 사용자를 로그인 페이지로 리다이렉트합니다.
 def index(request):
-    # 현재 로그인한 사용자와 연결된 이벤트들만 가져옵니다.
-    user_events = Events.objects.filter(user=request.user)
+    # 로그인한 사용자의 이벤트만 가져옵니다. 로그인하지 않았다면 모든 이벤트를 가져옵니다.
+    user_events = Events.objects.filter(user=request.user) if request.user.is_authenticated else Events.objects.all()
 
+    # 로그인 여부에 따라 템플릿에 변수 전달
     context = {
         "events": user_events,
+        "is_user_authenticated": request.user.is_authenticated,
     }
     return render(request, 'calendar.html', context)
 
 
+# 이벤트를 화면에 뿌려주는 함수
 @login_required(login_url='accounts/login/')
 def all_events(request):
     # 현재 로그인한 사용자와 연결된 이벤트들만 가져옵니다.
     user_events = Events.objects.filter(user=request.user)
-    out = []
+    out = [] # 배열 선언
     for event in user_events:
         out.append({
             'title': event.name,
@@ -46,7 +45,7 @@ def all_events(request):
 
     return JsonResponse(out, safe=False)
 
-
+# 이벤트를 추가할 때 동작하는 함수
 @login_required
 def add_event(request):
     start = request.GET.get("start", None)
@@ -59,7 +58,7 @@ def add_event(request):
     data = {}
     return JsonResponse(data)
 
-
+# 이벤트를 수정할 때 동작하는 함수
 @login_required
 def update(request):
     event_id = request.GET.get("id", None)
@@ -81,7 +80,7 @@ def update(request):
     data = {"status": "success"}
     return JsonResponse(data)
 
-
+# 이벤트를 삭제할 때 동작하는 함수
 @login_required
 def remove(request):
     event_id = request.GET.get("id", None)
