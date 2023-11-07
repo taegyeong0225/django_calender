54f614ee5bc59b46e41b8fb0396e6595fa5a4227
taegyeong0225 <202244042@itc.ac.kr>
2023-11-07 23:28:39 +0900

    11주차 시작

--- calender/models.py
+++ calender/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User # user 연결
 
 # Create your models here.
 class Events(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255,null=True,blank=True)
     start = models.DateTimeField(null=True,blank=True)
     end = models.DateTimeField(null=True,blank=True)
-
 
 
     class Meta:
