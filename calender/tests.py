fe2df0854f10ba7a1fc491fe76e054445077c653
taegyeong0225 <202244042@itc.ac.kr>
2023-11-21 21:44:23 +0900

    중간저장2

--- calender/tests.py
+++ calender/tests.py
@@ -5,6 +5,32 @@ from django.test import TestCase
 from .views import send_email_function
 from unittest.mock import patch, MagicMock
 
+# calender/tests.py
+
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
+
+# 로그인한 사용자의 이메일을 가져오는지 확인
+class UserEmailTest(TestCase):
+    def setUp(self):
+        # 테스트 사용자 생성
+        self.user = User.objects.create_user(username='testuser', email='test@example.com', password='12345')
+        self.client = Client()
+
+    def test_user_email(self):
+        # 사용자 로그인
+        self.client.login(username='testuser', password='12345')
+
+        # 로그인한 사용자의 이메일 주소 가져오기
+        user_email = self.user.email
+
+        # 이메일 주소 확인
+        self.assertEqual(user_email, 'test@example.com')
+
+# python manage.py test calender.tests.UserEmailTest
+
+# 자동 메일 전송이 되는지 확인
 class EmailTest(TestCase):
     @patch('calender.views.smtplib.SMTP')
     def test_send_email_function(self, mock_smtp):
@@ -15,7 +41,7 @@ class EmailTest(TestCase):
         # 함수 호출
         email = 'taegeong@naver.com'
         name = '테스트 재료'
-        date = '2023-11-25'
+        date = '2023-11-25 00:00:00'
         send_email_function(email, name, date)
 
         # 함수가 호출되었는지 검증
