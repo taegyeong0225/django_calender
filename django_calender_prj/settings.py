c99d1d5a3006f5fe2f639aa2dcfa7401b906994b
taegyeong0225 <202244042@itc.ac.kr>
2023-11-29 03:11:29 +0900

    스케줄링에러 잡음(use_tz) - 프로젝트 끝 (~plan a)

--- django_calender_prj/settings.py
+++ django_calender_prj/settings.py
@@ -129,13 +129,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
-USE_TZ = False
+# USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
